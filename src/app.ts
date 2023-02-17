7776722c913beb1e37c185d73c3a00ff36bdabe7
OctSquid <pqvs6k96@s.okayama-u.ac.jp>
2023-02-18 06:24:03 +0900

    ✨ bolt app の作成

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,19 @@
-import { exit } from 'process';
+import { App } from '@slack/bolt';
+import config from 'config';
+import { AddressInfo } from 'net';
 
-console.log('hello world');
-exit(0);
+const app = new App({
+  token: config.get('token'),
+  signingSecret: config.get('signingSecret'),
+});
+
+// Start your app
+(async () => {
+  const server = await app.start(process.env.PORT || 3000);
+
+  console.log(
+    `⚡️ Bolt app is running! Server port: ${
+      (server.address() as AddressInfo).port
+    }`,
+  );
+})();
